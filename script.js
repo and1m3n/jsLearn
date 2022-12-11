a8af4c00949f0a4fed8a49f1df8eb4314fc7e7a8
and1m3n <andrewfrontend@yandex.ru>
2022-12-11 21:56:24 +0300

    add Answeers user

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-const a = 2;
+let numberOfFilms = +prompt('сколько фильмов вы уже посмотрели?', 'введите число');
+let personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false,
+};
+
+let lastMovie = prompt('Один из последних просмотренных фильмов?', '');
+let ratingLastMovie = prompt('на сколько оцениваете его?', '');
+personalMovieDB.movies[lastMovie] = ratingLastMovie;
+lastMovie = prompt('Один из последних просмотренных фильмов?', '');
+ratingLastMovie = prompt('на сколько оцениваете его?', '');
+personalMovieDB.movies[lastMovie] = ratingLastMovie;
